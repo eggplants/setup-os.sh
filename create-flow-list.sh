3338b9d3026c1dac7ca89c41db36d2ba8264a2b6
eggplants <w10776e8w@yahoo.co.jp>
2022-06-26 11:25:38 +0900

    Create create-flow-list.sh

---
+++ create-flow-list.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if ! compgen -G "./*-setup.sh" &> /dev/null; then
+  echo "$0: target script is not found." >&2
+  exit 1
+fi
+
+echo "[checking with bash]"
+if ! bash -n ./*-setup.sh; then
+  exit 1
+fi
+
+echo "[checking with shellcheck]"
+if command -v shellcheck &> /dev/null; then
+  if ! shellcheck ./*-setup.sh; then
+    exit 1
+  fi
+fi
+
+: >> README.md
+sed '/^## Flow/,$d' README.md > _
+mv _ README.md
+
+echo '## Flow
+```txt
+'"$(
+  sed -n 's/^wait_enter  *(\1) *&&.*$/\1/p' ./*-setup.sh | tr -d \'
+)"'
+```'
