d602040141f4f2a8c487b7b2b4d2ff37f878d777
haruna <w10776e8w@yahoo.co.jp>
2024-11-14 04:59:33 +0900

    Update create-flow-list.sh

--- create-flow-list.sh
+++ create-flow-list.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-if ! compgen -G "./*-setup.sh" &> /dev/null; then
+if ! compgen -G "./setup-*.sh" &> /dev/null; then
   echo "$0: target script is not found." >&2
   exit 1
 fi
 
 echo "[checking with bash]"
-if ! bash -n ./*-setup.sh; then
+if ! bash -n ./setup-*.sh; then
   exit 1
 fi
 
 echo "[checking with shellcheck]"
 if command -v shellcheck &> /dev/null; then
-  if ! shellcheck ./*-setup.sh; then
+  if ! shellcheck ./setup-*.sh; then
     exit 1
   fi
 fi
@@ -24,6 +24,6 @@ mv _ README.md
 echo '## Flow
 ```txt
 '"$(
-  sed -n 's/^wait_enter  *(\1) *&&.*$/\1/p' ./*-setup.sh | tr -d \'
+  sed -n 's/^wait_enter  *(\1) *&&.*$/\1/p' ./setup-*.sh | tr -d \'
 )"'
 ```'
